ef89ff742db6418057704b9e795a31733070b003
Kshitij Bantupalli <kshitij.bantupalli@gmail.com>
2020-02-24 23:24:37 -0500

    Initial commit

---
+++ Code/Beginner/Bin2Decimal/bintodec.js
@@ -0,0 +1,10 @@
+
+
+// TODO : Function to process the binary to decimal.
+// Function to read input.
+
+const readline = require('readline').createInterface({
+    input: process.stdin
+});
+
+process.stdout.write("Hello")
