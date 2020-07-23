a02fc0ebbd8d5140d4618cfcb5872b81aaf655a7
Kshitij Bantupalli <kshitij.bantupalli@gmail.com>
2020-07-22 22:37:38 -0400

    Setting up routes

--- Projects/Angular/calculator-app/src/app/app.module.ts
+++ Projects/Angular/calculator-app/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { CalculatorComponent } from './calculator/calculator.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CalculatorComponent
   ],
   imports: [
     BrowserModule,
